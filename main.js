a543061983b2a8b9b500dfbdca6dd8ed89089f66
Ian <ianmutugi91@gmail.com>
2024-04-03 07:29:18 +0300

    simple liker app assignment

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden");
+
+  const emptyHearts = document.querySelectorAll(".like-glyph");
+
+  emptyHearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          if (!heart.classList.contains("activated-heart")) {
+            heart.classList.add("activated-heart");
+            heart.innerText = FULL_HEART;
+          } else {
+            heart.classList.remove("activated-heart");
+            heart.innerText = EMPTY_HEART;
+          }
+        })
+        .catch(error => {
+          errorModal.classList.remove("hidden");
+          const errorMessage = document.getElementById("modal-message");
+          errorMessage.innerText = error;
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
+
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
-
-// Your JavaScript code goes here!
-
-
-
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
